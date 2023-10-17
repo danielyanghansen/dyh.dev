7561dd10832ffd7bc78724c14a8fe8420a41674f
Daniel Yang Hansen <daniel.yang.hansen@gmail.com>
2023-10-17 15:20:35 +0200

    Implement working animation for 404NotFound
    Signed-off-by: Daniel Yang Hansen <daniel.yang.hansen@gmail.com>

--- src/pages/404NotFound/index.tsx
+++ src/pages/404NotFound/index.tsx
@@ -8,21 +8,23 @@ type NotFoundPageProps = {
 
 const NotFoundPage: React.FC<NotFoundPageProps> = () => {
     const grid404 = [
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', 'X', ' ', 'X', ' ', ' ', 'X', ' ', ' ', 'X', ' ', 'X', ' '],
-        [' ', 'X', ' ', 'X', ' ', 'X', ' ', 'X', ' ', 'X', ' ', 'X', ' '],
-        [' ', 'X', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X', 'X', 'X', ' '],
-        [' ', ' ', ' ', 'X', ' ', 'X', ' ', 'X', ' ', ' ', ' ', 'X', ' '],
-        [' ', ' ', ' ', 'X', ' ', ' ', 'X', ' ', ' ', ' ', ' ', 'X', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',],
+        [' ', 'X', ' ', ' ', 'X', ' ', ' ', 'X', 'X', ' ', ' ', 'X', ' ', ' ', 'X', ' ',],
+        [' ', 'X', ' ', ' ', 'X', ' ', 'X', ' ', ' ', 'X', ' ', 'X', ' ', ' ', 'X', ' ',],
+        [' ', 'X', 'X', 'X', 'X', ' ', 'X', ' ', ' ', 'X', ' ', 'X', 'X', 'X', 'X', ' ',],
+        [' ', ' ', ' ', ' ', 'X', ' ', 'X', ' ', ' ', 'X', ' ', ' ', ' ', ' ', 'X', ' ',],
+        [' ', ' ', ' ', ' ', 'X', ' ', ' ', 'X', 'X', ' ', ' ', ' ', ' ', ' ', 'X', ' ',],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',],
 
     ]
 
     const blockSideLength = 1;
-    const emptyMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
-    const filledMaterial = new THREE.MeshBasicMaterial({ color: 0x00ee11 });
+    const emptyMaterial = new THREE.MeshBasicMaterial({ color: 0xee1111 });
+    const filledMaterial = new THREE.MeshBasicMaterial({ color: 0x00eece });
     const blockGeometry = new THREE.BoxGeometry(blockSideLength, blockSideLength, blockSideLength);
     const blocks: THREE.Mesh[] = [];
+    const width = grid404[0].length;
+    const height = grid404.length;
 
     const canvasRef = useRef<HTMLCanvasElement>(null);
 
@@ -32,12 +34,13 @@ const NotFoundPage: React.FC<NotFoundPageProps> = () => {
             const canvas = canvasRef.current;
 
             //create blocks
-            grid404.forEach((row, i) => {
+            grid404.reverse().forEach((row, i) => {
                 return row.forEach((col, j) => {
                     const material = col === 'X' ? filledMaterial : emptyMaterial;
                     const block = new THREE.Mesh(blockGeometry, material);
-                    block.position.x = j * blockSideLength;
-                    block.position.y = i * blockSideLength;
+                    block.position.x = j * blockSideLength - width / 2 * blockSideLength;
+                    block.position.y = i * blockSideLength - height / 2 * blockSideLength;
+                    block.position.z = 0;
                     blocks.push(block);
                 })
             }
@@ -50,37 +53,52 @@ const NotFoundPage: React.FC<NotFoundPageProps> = () => {
                 canvas.clientWidth / canvas.clientHeight,
                 0.1,
                 100);
-            camera.position.z = 10;
+            camera.position.z = 8; 
+            camera.position.y = 2;
+            camera.position.x = 4;
             camera.lookAt(0, 0, 0)
 
             //create renderer
             const renderer = new THREE.WebGLRenderer({ canvas });
-            renderer.setSize(window.innerWidth, window.innerHeight);
+            renderer.setSize(canvas.clientWidth, canvas.clientHeight);
 
             //================================================================================================
             //add objects to scene
             blocks.forEach(block => scene.add(block));
-            //Set background to black
-            scene.background = new THREE.Color(0x000000);
+            //Set background to white
+            scene.background = new THREE.Color(0xffffff);
             //Add light
-            // const light = new THREE.PointLight(0xffffff, 1);
-            // light.position.set(5, 5, 5);
-            // scene.add(light);
+            const light = new THREE.PointLight(0xababab, 1);
+            light.position.set(5, 5, 5);
+            scene.add(light);
+
+            var t = 1;
+            const animate = () => {
+                requestAnimationFrame(animate);
+                t += 0.01;
+
+                blocks.forEach(block => {
+                    block.position.z = Math.sin(t + block.position.x);
+                });
+                
+                renderer.render(scene, camera);
+            }
+            animate();
 
 
         }
 
-    })
+    }, [canvasRef.current, grid404]);
 
 
-    
-    return ( //canvas not working
+
+    return (
         <div>
             <h1>404</h1>
             <canvas ref={canvasRef}></canvas>
         </div>
     );
-    
+
 }
 
 const NoPage = () => {
