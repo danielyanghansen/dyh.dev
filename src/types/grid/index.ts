e1822c7bb103e62687cee8459390630e74b58d78
Daniel Yang Hansen <daniel.yang.hansen@gmail.com>
2023-11-17 00:15:37 +0100

    More types
    Signed-off-by: Daniel Yang Hansen <daniel.yang.hansen@gmail.com>

--- src/types/grid/index.ts
+++ src/types/grid/index.ts
@@ -1,6 +1,28 @@
 export type GridInfo2D<T> = {
-  height: number;
-  width: number;
-  organization: 'rowMajor' | 'columnMajor';
+  metadata: {
+    height: number;
+    width: number;
+    organization: GridOrganization;
+  };
+
   grid: Array<Array<T>>;
+  functions: {
+    getElement?: (x: number, y: number) => T;
+    setElement?: (x: number, y: number, value: T) => void;
+    populateGrid?: (value: T) => void;
+
+    // This one might not be too useful
+    fillRange?: (
+      value: T,
+      xStart: number,
+      xEnd: number,
+      yStart: number,
+      yEnd: number,
+    ) => void;
+  };
 };
+
+export enum GridOrganization {
+  RowMajor = 'rowMajor',
+  ColumnMajor = 'columnMajor',
+}
