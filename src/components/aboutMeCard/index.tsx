f3aa03f35a32576b0d928dd23fdce92eeec45e12
Daniel Yang Hansen <daniel.yang.hansen@gmail.com>
2023-12-27 23:40:47 +0100

    Prettyfy About Me card with MoXin art
    Signed-off-by: Daniel Yang Hansen <daniel.yang.hansen@gmail.com>

--- src/components/aboutMeCard/index.tsx
+++ src/components/aboutMeCard/index.tsx
@@ -14,6 +14,12 @@ import {
 const profileImage = 'https://avatars.githubusercontent.com/u/55885035?v=4';
 const githubURL = 'https://github.com/danielyanghansen';
 
+const backgroundImage = '/images/moxin-autumn-river-1.png'; // Update the path accordingly
+
+const defaultBgColor = '#58202076';
+
+const defaultBorderColor = '3px  solid #ed636377';
+
 export const AboutMeCard: React.FC = () => {
   return (
     <Container maxWidth="md">
@@ -25,24 +31,47 @@ export const AboutMeCard: React.FC = () => {
           flexDirection: 'column',
           width: 'auto',
           maxWidth: 400,
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+
+          border: '3px  solid #fd6363',
         }}
         className="card"
       >
-        <CardHeader title="Hello there!" />
+        <CardHeader
+          title="Hello there!"
+          sx={{
+            color: 'white',
+            fontWeight: 'bold',
+            backgroundColor: defaultBgColor,
+            borderRadius: 3,
+            border: defaultBorderColor,
+          }}
+        />
         <CardMedia
           component="img"
           image={profileImage}
           sx={{
             boxShadow: 2,
             borderRadius: 6,
-            border: '3px dotted #c10ec7',
+            border: defaultBorderColor,
             objectFit: 'contain',
             marginBlock: 1,
-            maxWidth: 300,
+            maxWidth: 250,
             margin: 'auto',
+            marginTop: 2,
+            marginBottom: 2,
           }}
         />
-        <CardContent>
+        <CardContent
+          sx={{
+            backgroundColor: defaultBgColor,
+            borderRadius: 3,
+            color: 'white',
+            border: defaultBorderColor,
+          }}
+        >
           <Typography variant="h5">My name is Daniel</Typography>
           <Typography variant="body1" flexWrap={'revert'}>
             I am a 4th year Computer Science student at NTNU, Trondheim, and
@@ -50,7 +79,7 @@ export const AboutMeCard: React.FC = () => {
           </Typography>
           <Typography variant="body1"></Typography>
           <Link href={githubURL} target="_blank" rel="noopener noreferrer">
-            <Button variant="contained" color="secondary" sx={{ marginTop: 4 }}>
+            <Button variant="contained" color="warning" sx={{ marginTop: 4 }}>
               Visit My GitHub
             </Button>
           </Link>
